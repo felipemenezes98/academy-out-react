27fcd765e4b8b5ff4572af183c25a9069bcdd330
felipemenezes98 <99834184+felipemenezes98@users.noreply.github.com>
2025-10-02 17:50:12 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-semibold">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
